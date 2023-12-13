88b0e28749e03b5068deede5f9d0ff91020bc190
Andrew <azevedoa66@gmail.com>
2023-12-12 22:26:04 -0500

    Created schedule for day 1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const conferenceTeams = {
-    "North": ["Aliens", "Astronauts", "Blizzards", "Crabs", "Defenders", "Dragons", "Eagles", "Hammers", "Orgres", "Riot", "Rocks", "Stars", "Tigers", "Volcanoes", "Wind", "Zombies"],
+    "North": ["Aliens", "Astronauts", "Blizzards", "Crabs", "Defenders", "Dragons", "Eagles", "Hammers", "Ogres", "Riot", "Rocks", "Stars", "Tigers", "Volcanoes", "Wind", "Zombies"],
     "East": ["Bears", "Beavers", "Dribblers", "Fighters", "Flames", "Gnomes", "Ice", "Mustangs", "Sailors", "Skeletons", "Tornadoes", "Troopers", "Wasps", "Wings", "Wizards", "Wolves"],
     "South": ["Bats", "Comets", "Flash", "Gators", "Gladiators", "Hoopers", "Hustlers", "Longhorns", "Miners", "Monsters", "Mysteries", "Roses", "Sharks", "Snipers", "Silencers", "Whales"],
     "West": ["Ballers", "Crusaders", "Dinos", "Explorers", "Frogs", "Heros", "Jungle", "Knights", "Magicians", "Missiles", "Phantoms", "Pirates", "Scorpions", "Turtles", "Vipers", "Wave"]
@@ -7,7 +7,12 @@ const conferenceTeams = {
 
 const teams = {}
 
-function Team(pg, sg, sf, pf, c, wins, losses, standing) {
+const schedule = {
+    day: 1,
+    "1": [[1, 2], [8, 10], [9, 16], [7, 11], [3, 15], [6, 12], [4, 14], [5, 13]]
+}
+
+function Team(pg, sg, sf, pf, c, wins, losses, standing, teamNum) {
     this.pg = pg;
     this.sg = sg;
     this.sf = sf;
@@ -16,6 +21,7 @@ function Team(pg, sg, sf, pf, c, wins, losses, standing) {
     this.wins = wins;
     this.losses = losses;
     this.standing = standing;
+    this.teamNum = teamNum;
 }
 
 function createLeague() {
@@ -27,7 +33,7 @@ function createLeague() {
             let sf = Math.floor(Math.random() * (99 - 65) + 65);
             let pf = Math.floor(Math.random() * (99 - 65) + 65);
             let c = Math.floor(Math.random() * (99 - 65) + 65);
-            let team = new Team(pg, sg, sf, pf, c, 0, 0, i + 1);
+            let team = new Team(pg, sg, sf, pf, c, 0, 0, i + 1, i + 1);
             teams[key][conferenceTeams[key][i]] = team;
         }
     }
@@ -84,10 +90,46 @@ function displayStandings() {
         }
     }
 }
+
+function displaySchedule() {
+    let dayNum = schedule.day;
+    let dayStr = dayNum.toString();
+    for (const conference in teams) {
+        let conferenceDiv = document.createElement("div");
+        conferenceDiv.innerHTML = conference;
+        document.getElementById("schedule-grid").append(conferenceDiv);
+        for (let i = 0; i < schedule[dayStr].length; i++) {
+            let team1;
+            let record1;
+            let team2;
+            let record2;
+            for (const team in teams[conference]) {
+                if (teams[conference][team].standing === schedule[dayStr][i][0]) {
+                    team1 = team;
+                    record1 = "(" + teams[conference][team].wins + "-" + teams[conference][team].losses + ")";
+                    console.log(team);
+                } else if (teams[conference][team].standing === schedule[dayStr][i][1]) {
+                    team2 = team;
+                    record2 = "(" + teams[conference][team].wins + "-" + teams[conference][team].losses + ")";
+                    console.log(team);
+                }
+            }
+            let matchup = document.createElement("div");
+            matchup.innerHTML = team1 + " " + record1 + " vs. " + team2 + " " + record2;
+            document.getElementById("schedule-grid").append(matchup);
+        }
+    }
+}
+
+function simulateDay() {
+    schedule.day += 1;
+}
+
 function start() {
     createLeague();
     orderStandings();
     displayStandings();
+    displaySchedule();
     console.log(teams);
 }
 
